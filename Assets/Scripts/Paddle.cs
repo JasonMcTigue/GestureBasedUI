ca9661a5d8badcb0ba8b6cece1afcf76ec8916a4
Jason McTigue <jasonmctigue@live.ie>
2017-03-27 15:20:18 +0100

    kinect functionality

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿
+/*
+using UnityEngine;
 using System.Collections;
 
 public class Paddle : MonoBehaviour {
@@ -19,3 +21,33 @@ public class Paddle : MonoBehaviour {
 
 	}
 }
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour
+{
+	public float paddleSpeed = 1f;
+
+	private Vector3 playerPos = new Vector3(0, -9.5f, 0);
+
+	void Update()
+	{
+		float xPos = transform.position.x;
+
+		if (KinectManager.instance.IsAvailable)
+		{
+			xPos = KinectManager.instance.PaddlePosition;
+		}
+		else
+		{
+			xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+		}
+
+		playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);
+
+		transform.position = playerPos;
+	}
+}
+
