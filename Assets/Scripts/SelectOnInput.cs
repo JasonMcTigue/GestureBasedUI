e2dddb340cb05f5763aa3e0e1fc4b53724431f84
Jason McTigue <jasonmctigue@live.ie>
2017-03-29 20:56:36 +0100

    Added main menu using unity assests store

---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+
+//Checks for input from a controller
+public class SelectOnInput : MonoBehaviour
+{
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
